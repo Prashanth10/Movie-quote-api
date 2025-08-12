a215f43e009ba64071a427bc3f546a7d17827315
pg <prashanthg321@gmail.com>
2025-08-12 06:44:31 +0200

    Init

---
+++ src/main/kotlin/com/example/movie_quote_api/MovieQuoteApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.movie_quote_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MovieQuoteApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MovieQuoteApiApplication>(*args)
+}
